c57692e45f2527c622c54df25d9083bad022903c
orineeman <orineeman0@gmail.com>
2022-05-26 12:21:40 +0300

    exs

--- index.js
+++ index.js
@@ -1,31 +1,97 @@
-const textInput = document.querySelector(".text-input");
-const divButtons = document.querySelector(".div-buttons");
-const notFoundDiv = document.querySelector(".not-found-div");
-const arrAllfruits = ["gamba","apple", "banana", "orange"];
+const clicksDiv = document.querySelector(".clicks-div");
+const clicksP = document.querySelector(".clicks-p");
+const _body = document.querySelector("._body");
+const div1 = document.querySelector(".div1");
+const zevel = document.querySelector(".zevel");
+let conter = 1;
 
-display(arrAllfruits);
-
-function display(arr) {
-  divButtons.textContent = "";
-  if (arr.length === 0){
-    notFoundDiv.textContent = "no results found!";
-    notFoundDiv.classList.add("not-found-div2");
-  } else {
-    notFoundDiv.textContent = "";
-    notFoundDiv.classList.remove("not-found-div2");
-     for (let val of arr) {
-        const newFruitDiv = document.createElement("div");
-        divButtons.append(newFruitDiv);
-        const newBtnFruit = document.createElement("button");
-        newBtnFruit.classList.add("btn");
-        newBtnFruit.textContent = val;
-        newFruitDiv.append(newBtnFruit);
-  }}
-}
-
-textInput.oninput = function () {
-    const arrFound = arrAllfruits.filter(function (item) {
-    return item.startsWith(textInput.value);
-  });
-  display(arrFound);
+_body.onclick = function () {
+  clicksP.textContent = conter;
+  conter++;
 };
+
+zevel.onclick = function () {
+  const divs = document.querySelectorAll(".div1");
+  console.log(divs);
+  for (let div of divs) {
+    div.onclick = function () {
+      div.style.display = "none";
+    };
+  }
+};
+const lemala = document.querySelector(".lemala");
+const yemina = document.querySelector(".yemina");
+const smola = document.querySelector(".smola");
+const lemata = document.querySelector(".lemata");
+const zuz = document.querySelector(".zuz");
+
+zuz.style.top = "30px";
+zuz.style.left = "200px";
+lemata.onclick = function () {
+  let sizeAsInteger = parseInt(zuz.style.top);
+  sizeAsInteger = sizeAsInteger + 10;
+  zuz.style.top = sizeAsInteger + "px";
+};
+
+lemala.onclick = function () {
+  let sizeAsInteger = parseInt(zuz.style.top);
+  sizeAsInteger = sizeAsInteger - 10;
+  zuz.style.top = sizeAsInteger + "px";
+};
+
+smola.onclick = function () {
+  let sizeAsInteger = parseInt(zuz.style.left);
+  sizeAsInteger = sizeAsInteger + 10;
+  zuz.style.left = sizeAsInteger + "px";
+};
+
+yemina.onclick = function () {
+  let sizeAsInteger = parseInt(zuz.style.left);
+  sizeAsInteger = sizeAsInteger - 10;
+  zuz.style.left = sizeAsInteger + "px";
+};
+
+const balon = document.querySelector(".balon");
+const lemala2 = document.querySelector(".lemala2");
+const lemata2 = document.querySelector(".lemata2");
+
+balon.style.fontSize = "30px";
+lemala2.onclick = function () {
+  let sizeAsInteger = parseInt(balon.style.fontSize);
+  sizeAsInteger = sizeAsInteger * 1.1;
+  console.log(sizeAsInteger);
+  balon.style.fontSize = sizeAsInteger + "px";
+  if (sizeAsInteger > 100) {
+    balon.style.display = "none";
+    lemala2.style.display = "none";
+  }
+};
+lemata2.onclick = function () {
+  let sizeAsInteger = parseInt(balon.style.fontSize);
+  sizeAsInteger = sizeAsInteger * 0.9;
+  console.log(sizeAsInteger);
+  balon.style.fontSize = sizeAsInteger + "px";
+};
+
+const startSfira = document.querySelector(".start-sfira");
+let stopSfira = document.querySelector(".stop-sfira");
+const _in = document.querySelector("._in");
+const sfiraDiv = document.querySelector(".sfira");
+let num = 0;
+startSfira.onclick = function () {
+  num = _in.value;
+  let interval = setInterval(function sfiraLeahor() {
+    if (num > 0) {
+      _in.value = num - 1;
+      num--;
+      stopSfira.onclick = () => stopsfira(interval);
+    } else {
+      clearInterval(interval);
+      _in.value = "";
+    }
+  }, 1000);
+};
+
+function stopsfira(interval) {
+  clearInterval(interval);
+}
